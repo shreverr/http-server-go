98f11317bf45ced0d7b68ecd640e6fe15bb44372
Shreshth Verma <verma2007s@gmail.com>
2024-04-21 16:18:45 +0530

    feat: server now responds with 200 OK

--- main.go
+++ main.go
@@ -9,13 +9,20 @@ func main () {
 	fmt.Println("HTTP SERVER RUNNING")
 	ln, err := net.Listen("tcp", ":4221")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error binding to a port: %v\n", err)
 	}
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			fmt.Println(err)
+			fmt.Printf("Error accepting TCP connection: %v\n", err)
 		}
-		fmt.Println(conn)
+		
+		fmt.Printf("New TCP connection accepted: %v\n", conn)
+
+		n, err := conn.Read([]byte("GET / HTTP/1.1\r\n\r\n"))
+		fmt.Printf("Reading from TCP connection: %v\n", n)
+
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		// go conn.Close()
 	}
 }
