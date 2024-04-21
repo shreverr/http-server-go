40ce4c470016b8b63d9f454978c80fd42bfde267
Shreshth Verma <verma2007s@gmail.com>
2024-04-21 08:28:52 +0530

    feat: added port binding logic

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
 func main () {
 	fmt.Println("HTTP SERVER RUNNING")
+	ln, err := net.Listen("tcp", ":4221")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(conn)
+	}
 }
